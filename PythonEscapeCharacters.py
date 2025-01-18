d8a15b5cba1bf382b58ab82ed99e6eeea5df9fbf
Bagdat <amantabagdat9@gmail.com>
2025-01-18 12:58:07 +0500

    Added new print statement

---
+++ PythonEscapeCharacters.py
@@ -0,0 +1,14 @@
+txt = (
+    "We are the so-called \"Vikings\" from the north.\n"
+    "\\'   : Single Quote\n"
+    "\\\\  : Backslash\n"
+    "\\n   : New Line\n"
+    "\\r   : Carriage Return\n"
+    "\\t   : Tab\n"
+    "\\b   : Backspace\n"
+    "\\f   : Form Feed\n"
+    "\\ooo : Octal value\n"
+    "\\xhh : Hex value"
+)
+
+print(txt)
