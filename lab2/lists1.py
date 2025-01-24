789847191da8662e5cb81d2ce1f950b70e249428
Bagdat <amantabagdat9@gmail.com>
2025-01-24 14:26:26 +0500

    lists1

---
+++ lab2/lists1.py
@@ -0,0 +1,12 @@
+#1
+mylist = ["apple", "banana", "cherry"]
+#2
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+#3
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+#4
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
