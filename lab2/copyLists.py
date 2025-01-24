749468e594cbe06f4a67ff11052d1195365ebe27
Bagdat <amantabagdat9@gmail.com>
2025-01-24 14:49:26 +0500

    Python - Copy Lists

---
+++ lab2/copyLists.py
@@ -0,0 +1,21 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+
+
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+
