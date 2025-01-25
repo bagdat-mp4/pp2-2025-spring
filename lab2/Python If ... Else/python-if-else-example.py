6a70ea06c8ed8a04f97c3758f57685188b219ed1
Bagdat <amantabagdat9@gmail.com>
2025-01-25 14:13:13 +0500

    Python

---
+++ lab2/Python If ... Else/python-if-else-example.py
@@ -0,0 +1,141 @@
+# Мынау 1
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+# Мынау 2
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+# Мынау 3
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+# Мынау 4
+a = 1
+b = 1
+c = 2
+d = 2
+
+if a == b and c == d:
+    print("Hello")
+
+# Мынау 5
+a = 1
+b = 1
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+# Мынау 6
+a = 1
+b = 2
+c = 1
+d = 1
+
+if a == b or c == d:
+    print("Hello")
+
+# Мынау 7
+if 5 > 2:
+    print("Yes")
+
+# Мынау 8
+a = 2
+b = 5
+
+# Мынау 9
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES")
+
+# Мынау 10
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+# Мынау 11
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+# Мынау 12
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# Мынау 13
+if a > b: 
+    print("a is greater than b")
+
+# Мынау 14
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# Мынау 15
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# Мынау 16
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+
+# Мынау 17
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the conditions is True")
+
+# Мынау 18
+a = 33
+b = 200
+if not a > b:
+    print("a is NOT greater than b")
+
+# Мынау 19
+x = 41
+
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+
+# Мынау 20
+a = 33
+b = 200
+
+if b > a:
+    pass
+
+# Мынау 21
+print("YES") if a == b else print("NO")
