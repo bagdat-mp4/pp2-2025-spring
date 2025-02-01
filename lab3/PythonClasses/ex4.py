61d5c81db633ab435cbd28ca66f2d557b755227e
Bagdat <amantabagdat9@gmail.com>
2025-02-01 12:27:58 +0500

    show

---
+++ lab3/PythonClasses/ex4.py
@@ -0,0 +1,24 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x  # Нүктенің x координаты
+        self.y = y  # Нүктенің y координаты
+
+    def show(self):
+        print(f"Координаттар: ({self.x}, {self.y})")  # Координаттарды көрсету
+
+    def move(self, new_x, new_y):
+        self.x = new_x  # x координатын өзгерту
+        self.y = new_y  # y координатын өзгерту
+
+    def dist(self, other_point):
+        # Екі нүкте арасындағы қашықтықты есептеу
+        return math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
+
+# Мысал қолдану:
+p1 = Point(2, 3)
+p2 = Point(5, 7)
+p1.show()
+p2.show()
+print(p1.dist(p2))  # Шығару: 5.0
